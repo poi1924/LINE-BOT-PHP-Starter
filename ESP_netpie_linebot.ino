ae86973ed58b2060b9342998c83a0b01d384479f
poi1924 <45979451+poi1924@users.noreply.github.com>
2018-12-19 05:56:05 +0700

    Update ESP_netpie_linebot.ino

--- ESP_netpie_linebot.ino
+++ ESP_netpie_linebot.ino
@@ -2,17 +2,16 @@
 #include <MicroGear.h>
 #include <ESP8266HTTPClient.h>
 #include <ArduinoJson.h>
-const char* ssid     = "your SSID"; //change this to your SSID
-const char* password = "your PASSWORD"; //change this to your PASSWORD
+const char* ssid     = "CPH1611"; //change this to your SSID
+const char* password = "87654321"; //change this to your PASSWORD
 
-const char* host = "your linebot server";//change this to your linebot server ex.http://numpapick-linebot.herokuapp.com/bot.php
-#define APPID   "your APPID"     //change this to your APPID
-#define KEY     "your KEY"     //change this to your KEY
-#define SECRET  "your SECRET"     //change this to your SECRET
+const char* host = "https://fansetting.herokuapp.com/bot.php";//change this to your linebot server ex.http://numpapick-linebot.herokuapp.com/bot.php
+#define APPID   "snG1NLc4SIKthQu"     //change this to your APPID
+#define KEY     "6ujULYzJlwfD4NmQ2RZhPdIhy"     //change this to your KEY
+#define SECRET  "snG1NLc4SIKthQu:6ujULYzJlwfD4NmQ2RZhPdIhy"     //change this to your SECRET
 
 #define ALIAS   "NodeMCU1" //set name of drvice
 #define TargetWeb "switch" //set target name of web
-
 WiFiClient client;
 String uid = "";
 int timer = 0;
