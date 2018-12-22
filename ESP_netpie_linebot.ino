c65fd620c9fdf8779f66f651e724c98b3cd34e6b
poi1924 <45979451+poi1924@users.noreply.github.com>
2018-12-22 22:33:05 +0700

    Update ESP_netpie_linebot.ino

--- ESP_netpie_linebot.ino
+++ ESP_netpie_linebot.ino
@@ -6,7 +6,7 @@ const char* ssid     = "CPH1611"; //change this to your SSID
 const char* password = "87654321"; //change this to your PASSWORD
 
 const char* host = "https://fansetting.herokuapp.com/bot.php";//change this to your linebot server ex.http://numpapick-linebot.herokuapp.com/bot.php
-#define APPID   "fan/"     //change this to your APPID
+#define APPID   "fan"     //change this to your APPID
 #define KEY     "snG1NLc4SIKthQu"     //change this to your KEY
 #define SECRET  "6ujULYzJlwfD4NmQ2RZhPdIhy"     //change this to your SECRET
 
